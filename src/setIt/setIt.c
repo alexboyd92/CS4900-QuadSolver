8cebffc6aea6ffdb30ef32e3e91710b6e964ef95
Alexander <Alexanderboyd92@gmail.com>
2019-10-16 11:47:02 -0400

    Makefile for setIt

--- src/setIt/setIt.c
+++ src/setIt/setIt.c
@@ -1,4 +1,8 @@
-#include "setIT.h"
+#include "setIt.h"
+int main(int argc, char const *argv[]) {
+	printf("%s\n","You shouldnt be here" );
+	return 0;
+}
 void  printResults(int flag,double a,double b){
 	switch (flag) {
 	case 0:
--- src/setIt/setIt.h
+++ src/setIt/setIt.h
@@ -1,2 +1,5 @@
 #include <stdio.h>
+#ifndef SETIT_H
+#define SETIT_H
 extern void  printResults(int flag,double a,double b);
+#endif
