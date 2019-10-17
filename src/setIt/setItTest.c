20c77da8d210e101ce6bd68fbcd9e9e4c3610a7a
Alexander <Alexanderboyd92@gmail.com>
2019-10-17 09:38:18 -0400

    Some testing on print results

--- src/setIt/setItTest.c
+++ src/setIt/setItTest.c
@@ -1,7 +1,17 @@
 #include "../setIt/setIt.h"
-#include <CUnit/CUnit.h>
+#include "../testing/cunit.h"
 int main(int argc, char const *argv[]) {
-	/* code */
-	printResults(2,20.20);
+	// check if no real roots
+	printResults(2,20,21);
+	// check if two real roots
+	printResults(1,20,21);
+	//check if one real root
+	printResults(0,20,20);
+	// check if error
+	printResults(3,20,20);
+	// check for negative results
+	printResults(1,-120,20);
+	// check for higher than a double
+	printResults(1,DBL_MAX+2,1);
 	return 0;
 }
