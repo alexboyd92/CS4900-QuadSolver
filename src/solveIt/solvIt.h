f68ab2c9de8caa52ef91ebd8cfe2c148c64fbbed
Alexander <Alexanderboyd92@gmail.com>
2019-10-18 15:29:39 -0400

    basic layout of solveIt

---
+++ src/solveIt/solvIt.h
@@ -0,0 +1,6 @@
+
+#ifndef SOLVEIT_H
+#define SOLVEIT_H
+extern int solve( double *a, double *b, double *c);
+
+#endif
---
+++ src/solveIt/solveIt.c
@@ -0,0 +1,7 @@
+#include "solveIt.h"
+#define  NO_ROOTS 2
+#define TWO_ROOTS 1
+#define ONE_ROOT  0
+int solve( double *a, double *b, double *c){
+
+}
