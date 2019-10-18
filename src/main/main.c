846b956948d53f1061136bb48c3d39358e75338d
Alexander <Alexanderboyd92@gmail.com>
2019-10-18 13:00:17 -0400

    more sudo code

--- src/main/main.c
+++ src/main/main.c
@@ -1,15 +1,27 @@
 #include "main.h"
 // Main //
 int main(int argc, char const *argv[]) {
+	int quit=0;
 	// if argc == 4 call validate input
 	//call solve
 	// call print results
+	// do while if no arguments are specified
+	// if argc is 1
+	do {
+		// call get input
+		// then validate input
+		// if validate fails call help
+		// else if input = q quit =1
+		//else
+		//call solve
+		// call printResults
 
-	// If argc is 1 call get input
-	// then validate input
-	//call solve
-	// call printResults
+
+	} while(!quit);
+
+
 	// if argc > 4 call  help
+	//exit
 
 
 
