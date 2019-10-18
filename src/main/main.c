2781b4a4e18c1a34137d0a4af4f95e8f009721c7
Alexander <Alexanderboyd92@gmail.com>
2019-10-18 12:40:33 -0400

    Started sudo code

--- src/main/main.c
+++ src/main/main.c
@@ -1,6 +1,18 @@
 #include "main.h"
-// Main
+// Main //
 int main(int argc, char const *argv[]) {
+	// if argc == 4 call validate input
+	//call solve
+	// call print results
+
+	// If argc is 1 call get input
+	// then validate input
+	//call solve
+	// call printResults
+	// if argc > 4 call  help
+
+
+
 
 	return 0;
-}
+}// End main//
