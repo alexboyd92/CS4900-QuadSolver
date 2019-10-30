a97d0eeaa993c109f9839d17bdf02efebcd9d137
Sean Kerrigan <sean.g.kerrigan@wmich.edu>
2019-10-30 19:28:04 -0400

    added solveitTest

---
+++ src/testing/solveitTest.c
@@ -0,0 +1,23 @@
+#include "../setIt/solveIt.h"
+#include "../testing/cunit.h"
+int main(int argc, char const *argv[]) {
+								int a= 1, b = 10, c = 1;
+								double ad = 1.0, bd=4.0, cd=1.0;
+								float af=10, bf=100.0000004, cf=10.000006;
+								char ch = 'a';
+
+								// check if NAN
+								solveIt('abcde',20,21);
+								// check if two real roots
+								solveIt(1,10,4);
+								//check if one real root
+								solveIt(1,4,4);
+								// check if error
+								solveIt(3,1,20);
+								// check for negative results
+								solveIt(1,-120,20);
+								// check for higher than a double
+								solveIt(1,DBL_MAX+2,1);
+								solveIt(&a, &b, &c)
+								return 0;
+}
