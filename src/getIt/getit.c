cdb023a60ad29d4a12e09d1b9b03462c6e267022
Alexander <Alexanderboyd92@gmail.com>
2019-10-30 18:37:54 -0400

    deleted .o's

--- src/getIt/getit.c
+++ src/getIt/getit.c
@@ -3,17 +3,17 @@
 //---GetValues---//
 char *  GetValues(){
 
-  // buffsize limit 100
-  char buf[BUFFERSIZE];
-  //input returned to calling function
-  char * input;
+	// buffsize limit 100
+	char buf[BUFFSIZE];
+	//input returned to calling function
+	char * input;
 
 
 
 
 	printf("Enter values: ");
 
-  buf == fgets(buf,BUFFERSIZE,stdin);
+	buf == fgets(buf,BUFFSIZE,stdin);
 
 
 	input = (char *)malloc(strlen(buf) +1);
