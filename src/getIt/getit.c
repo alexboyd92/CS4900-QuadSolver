7de84f819d2a919625b6d464741522317fa699a1
mtwyan <matthew.t.wyant@wmich.edu>
2019-11-02 17:46:44 -0400

    no changes

--- src/getIt/getit.c
+++ src/getIt/getit.c
@@ -1,24 +1,21 @@
 #include "getit.h"
 
 //---GetValues---//
-char *  GetValues(){
-
+char *  GetValues(int flag){
         // buffsize limit 100
         char buf[BUFFERSIZE];
         //input returned to calling function
         char * input;
-
         printf("Enter values: ");
 
-        buf == fgets(buf,BUFFERSIZE,stdin);
+        fgets(buf,BUFFERSIZE,stdin);
 
         input = (char *)malloc(strlen(buf) +1);
 
         //if logging enabled output to file.
         if(input == NULL) {
-                printf("Unable to allocate Memory");
+                printf("Unable to allocate Memory\n");
         }
-
         strcpy(input,buf);
 
         return input;
