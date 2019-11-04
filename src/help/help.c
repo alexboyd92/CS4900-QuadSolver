b75de6f3b53c6e4ec91fac53c4144416c9c0e766
mtwyan <matthew.t.wyant@wmich.edu>
2019-11-04 17:48:55 -0500

    added STCMP to main that doesnt work yet.

--- src/help/help.c
+++ src/help/help.c
@@ -3,7 +3,7 @@
 
 
 void userTypedHelp(){//This is the message the user gets when they type help
-	printf("You must type 3 roots separated by commas or spaces like so:\n 1,2,3 or 1 2 3" );
+	printf("You must type 3 roots separated by spaces like so:\n 1.0 2 3.1234 or 5e10 3.14 6.3e-4\n" );
 
 
 
