f9820a6720bb3ce7d0b79602a29f26a964c672a8
Alexander <Alexanderboyd92@gmail.com>
2019-11-05 18:19:25 -0500

    Flusshing buffer

--- src/getIt/getit.c
+++ src/getIt/getit.c
@@ -2,25 +2,26 @@
 
 //---GetValues---//
 char *  GetValues(int flag,FILE * logFile){
-        // buffsize limit 100
-        char buf[BUFFERSIZE];
-        //input returned to calling function
-        char * input;
-        printf("Enter values: ");
+	// buffsize limit 100
+	char buf[BUFFERSIZE];
+	//input returned to calling function
+	char * input;
+	printf("Enter values: ");
 
-        fgets(buf,BUFFERSIZE,stdin);
+	fgets(buf,BUFFERSIZE,stdin);
+	while ( getchar() != '\n' );
 
-        input = (char *)malloc(strlen(buf) +1);
-        //if logging enabled output to file.
-        if(input == NULL) {
-          if(flag == 1){
-                fprintf(logFile,"Unable to allocate Memory\n");
-          }
-        }
-        else if(flag == 1){
-          fprintf(logFile,"Input from user is %s\n", buf);
-        }
+	input = (char *)malloc(strlen(buf) +1);
+	//if logging enabled output to file.
+	if(input == NULL) {
+		if(flag == 1) {
+			fprintf(logFile,"Unable to allocate Memory\n");
+		}
+	}
+	else if(flag == 1) {
+		fprintf(logFile,"Input from user is %s\n", buf);
+	}
 
-        strcpy(input,buf);
-        return input;
+	strcpy(input,buf);
+	return input;
 }//END getValues()
