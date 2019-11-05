32e7e95b42e7c30f3e94f6d413428929053f86cc
Alexander <Alexanderboyd92@gmail.com>
2019-11-05 12:17:42 -0500

    changed logging to log

--- src/help/help.c
+++ src/help/help.c
@@ -2,21 +2,12 @@
 #include "help.h"
 
 void userTypedHelp(){//This is the message the user gets when they type help
-<<<<<<< HEAD
-	printf("You must type 3 roots separated by spaces like so:\n 1.0 2 3.1234 or 5e10 3.14 6.3e-4\n" );
+	printf("This program will solve a quadratic equation (ax^2+bx+c=0). You must type in three numbers (a,b, and c) separated by a space\n");
+	printf("Examples of acceptable input:\n");
+	printf("1 2 3\n");
+	printf("500 3 26\n");
+	printf("4900 765 10000\n");
 
-
-
-
-=======
-printf("This program will solve a quadratic equation (ax^2+bx+c=0). You must type in three numbers (a,b, and c) separated by a space\n");
-printf("Examples of acceptable input:\n");
-printf("1 2 3\n");
-printf("500 3 26\n");
-printf("4900 765 10000\n");
-
-printf("If you want the results outputted to a logging file, type logging\n");
-printf("If you want to exit this program, type q\n" );
->>>>>>> c95667cbb8b8a1f0b4ddbffa74849c855f7665fd
+	printf("If you want the results outputted to a logging file, type log\n");
+	printf("If you want to exit this program, type q\n" );
 }
-  
