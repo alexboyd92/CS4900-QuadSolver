b18bdfac9e61af450c385abba0ed5ba279346b31
Sean Kerrigan <sean.g.kerrigan@wmich.edu>
2019-11-05 00:42:18 -0500

    loging should work **not tested**

--- src/solveIt/solveIt.c
+++ src/solveIt/solveIt.c
@@ -3,23 +3,18 @@
 #define TWO_ROOTS 1
 #define ONE_ROOT  0
 int solve( double *a, double *b, double *c, int logging){
-								//double *solveVal1,* solveVal2;
 								double a2 = *a, b2 = *b, c2=*c;
 								double rootVal = pow(b2, 2)-(4.0*a2*c2);
 								if(logging==1) {
 																fprintf(logFile, "value of that is in the "
 																								"sqrt %f \n ",rootVal);
 								}
-
-								//solveVal1 = solveVal2 = malloc(sizeof(double)); // this needs to be freeded at some point
 								//if statement to check if SQRT(Negitive or Nan or +-infinite)
 								if((rootVal<0 || fpclassify(rootVal) == FP_NAN || fpclassify(rootVal) == FP_INFINITE) && a2!= 0) {
-																//printf("root is complex\n");
-
-
 																return NO_ROOTS;
-								}         //end if
-								          //if statement to check if SQRT(is 0)
+								}
+								//end if
+								//if statement to check if SQRT(is 0)
 								else if(rootVal==0) {
 																*a = -b2/(2.0 * a2);
 																float f = (float)*a;
@@ -34,8 +29,6 @@ int solve( double *a, double *b, double *c, int logging){
 
 																								printf("Just a heads up!!\n With the numbers you put in there could be a rounding error with your output\n" );
 																}
-																//printf("roots are the same\n");
-																//printf("Root of quadratic equation is:%.3f\n", solveVal1);
 
 																return ONE_ROOT;
 								}         //end if
@@ -62,7 +55,6 @@ int solve( double *a, double *b, double *c, int logging){
 
 																								printf("Just a heads up!!\n With the numbers you put in there could be a rounding error with your output\n" );
 																}
-																//printf("Roots of quadratic equation are:%.3f , %.3f", solveVal1, solveVal2);
 																return TWO_ROOTS;
 								}        // end of else
 
