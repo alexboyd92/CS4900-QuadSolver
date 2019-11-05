835df04a4ecb2dd777a64599a087c846ddad5146
Alexander <Alexanderboyd92@gmail.com>
2019-11-05 12:22:42 -0500

    unfixed the fix that broke things

--- src/main/main.c
+++ src/main/main.c
@@ -77,40 +77,40 @@ int main(int argc, char const *argv[]) {
 
 				}
 				else {
-					// then validate input
-					if(logging==1) {
-						logFile=fopen(writeLog,"a");
-						//	fprintf(logFile, "Entering validate" );
-					}
 
-					validateFlag = Validate(input, logging, logFile, &a, &b, &c);
-					// if validate fails call help
-					if(validateFlag==-1) {
-						//invalid input;
-						userTypedHelp();
-					}
-					else{
-						//call solve
-						if(logging==1) {
-							fprintf(logFile, "Entering solve with value of "
-							        "a:%.4f b:%.4f c:%.4f\n",a,b,c );
-						}
-						flag= solve(&a,&b,&c,logging,logFile);
-
-						// call print results
-						if(logging==1) {
-							fprintf(logFile, "Entering print results "
-							        "with values of a:%.4f b:%.4f c: %.4f and "
-							        "The flag variable set to :%d\n", a,b,c,flag);
-
-						}
-						printResults( flag,a,b,logFile,logging);
-					}        //end else
 				}
 			}
 
 
+			// then validate input
+			if(logging==1) {
+				logFile=fopen(writeLog,"a");
+				//	fprintf(logFile, "Entering validate" );
+			}
 
+			validateFlag = Validate(input, logging, logFile, &a, &b, &c);
+			// if validate fails call help
+			if(validateFlag==-1) {
+				//invalid input;
+				userTypedHelp();
+			}
+			else{
+				//call solve
+				if(logging==1) {
+					fprintf(logFile, "Entering solve with value of "
+					        "a:%.4f b:%.4f c:%.4f\n",a,b,c );
+				}
+				flag= solve(&a,&b,&c,logging,logFile);
+
+				// call print results
+				if(logging==1) {
+					fprintf(logFile, "Entering print results "
+					        "with values of a:%.4f b:%.4f c: %.4f and "
+					        "The flag variable set to :%d\n", a,b,c,flag);
+
+				}
+				printResults( flag,a,b,logFile,logging);
+			}        //end else
 
 		} while(quit == 0); //end do while loop
 
