526d140e14300b63831150cd178c8260e3a69351
Sean Kerrigan <sean.g.kerrigan@wmich.edu>
2019-11-05 18:57:32 -0500

    buff dude problem solved

--- src/getIt/getit.c
+++ src/getIt/getit.c
@@ -2,26 +2,27 @@
 
 //---GetValues---//
 char *  GetValues(int flag,FILE * logFile){
-	// buffsize limit 100
-	char buf[BUFFERSIZE];
-	//input returned to calling function
-	char * input;
-	printf("Enter values: ");
+								// buffsize limit 100
+								char buf[BUFFERSIZE];
+								//input returned to calling function
+								char * input;
+								printf("Enter values: ");
 
-	fgets(buf,BUFFERSIZE,stdin);
-	//while ( getchar() != '\n' );
+								fgets(buf,BUFFERSIZE,stdin);
+								//while ( getchar() != '\n' );
+								__fpurge (stdin);
 
-	input = (char *)malloc(strlen(buf) +1);
-	//if logging enabled output to file.
-	if(input == NULL) {
-		if(flag == 1) {
-			fprintf(logFile,"Unable to allocate Memory\n");
-		}
-	}
-	else if(flag == 1) {
-		fprintf(logFile,"Input from user is %s\n", buf);
-	}
+								input = (char *)malloc(strlen(buf) +1);
+								//if logging enabled output to file.
+								if(input == NULL) {
+																if(flag == 1) {
+																								fprintf(logFile,"Unable to allocate Memory\n");
+																}
+								}
+								else if(flag == 1) {
+																fprintf(logFile,"Input from user is %s\n", buf);
+								}
 
-	strcpy(input,buf);
-	return input;
+								strcpy(input,buf);
+								return input;
 }//END getValues()
