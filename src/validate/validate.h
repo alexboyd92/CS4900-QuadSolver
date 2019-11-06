4bb118cda394f5259211b50fff23dd70b8abc877
alexboyd92 <alexboyd92@github.com>
2019-11-06 18:44:50 -0500

    Fixed validate test

--- src/validate/validate.h
+++ src/validate/validate.h
@@ -1,11 +1,11 @@
+
+#ifndef VALIDATE_H
+#define VALIDATE_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
 #include <math.h>
-
-#ifndef VALIDATE_H
-#define VALIDATE_H
 
 extern int Validate (char * input, int flag, FILE * logFile, double* a, double* b, double* c);
 extern int Tokenize(int flag, FILE * logFile, char * input, double* a, double* b, double* c);
