285baec3e71df95123585e18c4a9be394131f7fc
Daniel Musallam <Daniel Musallam>
2019-11-07 10:52:46 -0500

    more unit tests for validate

--- src/testing/validateTest.c
+++ src/testing/validateTest.c
@@ -7,9 +7,9 @@
 int main(){
 
 
-//Test to make sure validate returns -1 with bad input
+//Test to make sure validate returns -1 with buffer overflow
 
-	char input[2000] ="-1 ";
+	char input[2000] ="11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 1 1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
 	FILE * d=NULL;
 	double a=0;
 	double b=0;
@@ -17,7 +17,28 @@ int main(){
 
 	int checker= Validate(input,0,d,&a,&b,&c);
 
-	assertd_eq("checker", checker, 0);
+	assertd_eq("checker", checker, -1);
+
+
+//Test to make sure validate returns -1 with random characters
+
+	char input2[2000]="abc def gfjwlafjasd;lkfjewofjsdkdsmklfjadskleh";
+
+	checker= Validate(input2,0,d,&a,&b,&c);
+
+	assertd_eq("checker", checker,-1);
+
+
+
+
+//Test to make sure validate returns -1 when input is too short( <5)
+	char input3[2000]="1234";
+
+	checker= Validate(input3,0,d,&a,&b,&c);
+
+	assertd_eq("checker", checker,-1);
+
+
 
 
 
